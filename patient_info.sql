eabbfcb58e3e767a185c04257f6246ede9e321ee
tony2265 <tony22657833@gmail.com>
2021-03-31 16:33:50 +0800

    patient_info

--- patient_info.sql
+++ patient_info.sql
@@ -1,3 +1,18 @@
+DROP TABLE IF EXISTS patient_info;
+
 create table patient_info(
-	
+    patient_id varchar(10) not NULL,
+    sex varchar(10),
+    age int,
+    province varchar(20),
+    city varchar(20),
+    infection_case varchar(100),
+    primary key (patient_id)
 );
+
+load data local infile './patient_info.csv'
+into table patient_info
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 lines;
